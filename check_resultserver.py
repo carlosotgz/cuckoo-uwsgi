e8e253559d5cbce9bd31bb97bdc8b5444ab4f2af
Carlos Ortigoza Dempster <carlos.otgz@gmail.com>
2017-05-26 16:57:24 +0200

    Improving some functions

--- check_resultserver.py
+++ check_resultserver.py
@@ -5,20 +5,19 @@ import ConfigParser
 import socket
 import time
 
-def check_tcp_port(address, port):
-    s = socket.socket()
-    try:
-        s.connect((address, port))
-        return True
-    except socket.error, e:
-        return False
-
-def check_service(address, port, name):
+"""
+Knock a TCP port once per second, 20 seconds at most.
+"""
+def check_tcp_port(address, port, name):
     for attempt in range(20):
-        if check_tcp_port(address, port):
+        s = socket.socket()
+        try:
+            s.connect((address, port))
             print "{0} is ready".format(name)
-            return
-        time.sleep(1)
+            s.close()
+            return True
+        except socket.error, e:
+            time.sleep(1)
     else:
         print "{0} is not available".format(name)
         sys.exit(1)
@@ -28,6 +27,6 @@ cuckoo_cfg.read("/cuckoo/conf/cuckoo.conf")
 
 resultserver_ip =  cuckoo_cfg.get('resultserver', 'ip')
 resultserver_port =  int(cuckoo_cfg.get('resultserver', 'port'))
-check_service(resultserver_ip, resultserver_port, "Result server")
+check_tcp_port(resultserver_ip, resultserver_port, "Result server")
 
 sys.exit()
